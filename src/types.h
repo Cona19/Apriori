fa655d1fdf977861c38ed80f55e179ec4327c3f0
Cona19 <hyeonso5@gmail.com>
2016-03-30 10:46:26 +0900

    add candidate_map type

--- src/types.h
+++ src/types.h
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <vector>
 #include <set>
+#include <map>
 
 typedef int item_t;
 typedef std::set<item_t> trx_t;
+typedef std::vector<trx_t> trx_list_t;
+typedef std::map<trx_t, int> cand_map_t;
